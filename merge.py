278530f6d01da11efc84ed305846a6ff1cfe3142
Conrad <conradleonik@gmail.com>
2019-07-06 13:58:31 -0500

    updated added.xlsx with Chris 's csv file, the four polarity-instruction combinations with strings and numbers

--- merge.py
+++ merge.py
@@ -24,6 +24,10 @@ combined = pd.merge(psych_1, psych_2, how='outer', on=psych_1.columns[0])
 combined = pd.merge(combined, psych_3, how='outer', on=psych_1.columns[0])
 #reading the list of subjects that we care about
 n = pd.read_excel("NFB analysis subjects 7.5.19.xlsx")
+#read trials csv files
+trials = pd.read_csv("subj_groups_and_corr.csv")
+#merge trials into the n subjects
+n = pd.merge(n, trials, how='outer', on=psych_1.columns[0])
 #filtering out subjects that we don't care about
 filtered = pd.merge(n, combined, how='left', on=n.columns[0])
 #replace blank values or ones that contain ! or ~ with -9999
@@ -31,11 +35,8 @@ filtered.replace(to_replace=r'.*!+.*', value="-9999", regex=True, inplace=True)
 filtered.replace(to_replace=r'.*~+.*', value="-9999", regex=True, inplace=True)
 filtered.fillna('-9999', inplace=True)
 print(filtered)
-#read trials csv files
-trials = pd.read_csv("test.csv")
-added = pd.merge(filtered, trials, how='outer', on=psych_1.columns[0])
 #output excel file with one combined, filtered, added table (but without order indicator)
-added.to_excel("added.xlsx")
+filtered.to_excel("added.xlsx")
 """
 ids = [SUB.id]
 test = table(ids, NPEP_grand_linear(:,1,1), NPEP_grand_linear(:,2,1), NPEP_grand_linear(:,3,1), NPEP_grand_linear(:,4,1), NPEP_grand_linear(:,5,1), NPEP_grand_linear(:,6,1), NPEP_grand_linear(:,7,1), NPEP_grand_linear(:,8,1), NPEP_grand_linear(:,9,1), NPEP_grand_linear(:,10,1), NPEP_grand_linear(:,11,1), NPEP_grand_linear(:,12,1), NPEP_grand_linear(:,1,2), NPEP_grand_linear(:,2,2), NPEP_grand_linear(:,3,2), NPEP_grand_linear(:,4,2), NPEP_grand_linear(:,5,2), NPEP_grand_linear(:,6,2), NPEP_grand_linear(:,7,2), NPEP_grand_linear(:,8,2), NPEP_grand_linear(:,9,2), NPEP_grand_linear(:,10,2), NPEP_grand_linear(:,11,2), NPEP_grand_linear(:,12,2))
